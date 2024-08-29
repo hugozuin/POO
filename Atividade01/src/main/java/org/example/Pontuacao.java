36efd808b03ad238015b523b3e3793a33e75f8f5
Hugo Zuin <hugo.zuin@hotmail.com>
2024-08-29 17:47:33 -0300

    Update Pontuacao.java

--- Atividade01/src/main/java/org/example/Pontuacao.java
+++ Atividade01/src/main/java/org/example/Pontuacao.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Pontuacao {
+
+    public int pontos;
+    public int pontuacao;
+
+    public Pontuacao(int pontos, int pontuacao) {
+        this.pontos = pontos;
+        this.pontuacao = pontuacao;
+    }
 }

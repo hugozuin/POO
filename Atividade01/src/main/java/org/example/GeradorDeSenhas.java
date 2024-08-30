c38a6e65174b575dfcdc2af3c2c1e81e23e01995
Hugo Zuin <hugo.zuin@hotmail.com>
2024-08-30 14:15:25 -0300

    Update GeradorDeSenhas.java

--- Atividade01/src/main/java/org/example/GeradorDeSenhas.java
+++ Atividade01/src/main/java/org/example/GeradorDeSenhas.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import java.security.SecureRandom;
+import java.util.Random;
 
 public class GeradorDeSenhas {
 
@@ -9,35 +9,35 @@ public class GeradorDeSenhas {
     private static final String NUMEROS = "0123456789";
     private static final String SIMBOLOS = "!@#$%^&*()-_=+[]{}|;:',.<>?/";
 
-    private SecureRandom random = new SecureRandom();
+    private Random random = new Random();
 
     public String gerarSenha(int comprimento, boolean incluirMinusculas, boolean incluirMaiusculas, boolean incluirNumeros, boolean incluirSimbolos) {
-        StringBuilder caracteresDisponiveis = new StringBuilder();
+        String caracteresDisponiveis = "";
 
+        // Adicionando os tipos de caracteres selecionados pelo usuário à string de caracteres disponíveis
         if (incluirMinusculas) {
-            caracteresDisponiveis.append(LETRAS_MINUSCULAS);
+            caracteresDisponiveis += LETRAS_MINUSCULAS;
         }
         if (incluirMaiusculas) {
-            caracteresDisponiveis.append(LETRAS_MAIUSCULAS);
+            caracteresDisponiveis += LETRAS_MAIUSCULAS;
         }
         if (incluirNumeros) {
-            caracteresDisponiveis.append(NUMEROS);
+            caracteresDisponiveis += NUMEROS;
         }
         if (incluirSimbolos) {
-            caracteresDisponiveis.append(SIMBOLOS);
+            caracteresDisponiveis += SIMBOLOS;
         }
 
-        if (caracteresDisponiveis.length() == 0) {
+        if (caracteresDisponiveis.isEmpty()) {
             throw new IllegalArgumentException("Pelo menos uma categoria de caracteres deve ser selecionada.");
         }
 
-        StringBuilder senha = new StringBuilder(comprimento);
-
+        String senha = "";
         for (int i = 0; i < comprimento; i++) {
             int index = random.nextInt(caracteresDisponiveis.length());
-            senha.append(caracteresDisponiveis.charAt(index));
+            senha += caracteresDisponiveis.charAt(index);
         }
 
-        return senha.toString();
+        return senha;
     }
 }
